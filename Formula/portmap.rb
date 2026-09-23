1bc58e421c8bf8c69ade656c52534b19a9aad23a
t <t@t>
2026-09-23 09:53:03 +0800

    portmap V0.5

--- Formula/portmap.rb
+++ Formula/portmap.rb
@@ -3,8 +3,8 @@ class Portmap < Formula
 
   desc "Branch-scoped port allocator and Traefik label generator for docker-compose"
   homepage "https://github.com/ylang-ylang/portmap"
-  url "https://github.com/ylang-ylang/portmap/archive/refs/tags/V0.4.tar.gz"
-  sha256 "429a73e275629625143ee438d92114be328586814f1e6ad777f0102137e09b36"
+  url "https://github.com/ylang-ylang/portmap/archive/refs/tags/V0.5.tar.gz"
+  sha256 "8278eb2868c17d73fc7115f3fa6c5fc74c2169e2a45fa6b330278eef06ceea0c"
   license "MIT"
 
   depends_on "python@3.12"
